3baecca8a767272fa595837d72fc703e5cd0c3ad
bozo1999 <bozo.paragon@gmail.com>
2021-09-18 10:32:12 +0200

    Dodana cijela vježba Day05

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code that this line
+    var lastName = "Doe";
+    var lastNameLength = lastName.length;
+    // Only change code above this line
+    return lastNameLength;
+}
+console.log(main());
+module.exports = main;
---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myAdjective = "big";
+    var myVerb = "ran";
+    var myAdverb = "quickly";
+    var wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+
+    // Only change code above this line
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
